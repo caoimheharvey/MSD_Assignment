f3ec8a14823ce8e0702abd49e3964bb1ec6e454c
caoimheharvey <caoimhe.e.harvey@gmail.com>
2016-10-28 19:04:04 +0100

    0.0.3 Basic Skeleton

--- app/src/main/java/com/caoimheharv/msd_assignment/MainActivity.java
+++ app/src/main/java/com/caoimheharv/msd_assignment/MainActivity.java
@@ -1,13 +1,54 @@
 package com.caoimheharv.msd_assignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] temp = {1234, 0000, 1984};
+
+    Button verify;
+    EditText passcode;
+    int pin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        verify = (Button) findViewById(R.id.contBtn);
+        passcode = (EditText) findViewById(R.id.passcode);
+
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(passcode.getText().toString().isEmpty())
+                    Toast.makeText(MainActivity.this, "MUST ENTER PIN", Toast.LENGTH_SHORT).show();
+                else
+                    pin = Integer.parseInt(passcode.getText().toString());
+
+
+                boolean check = false;
+                for (int i = 0; i < temp.length; i++)
+                {
+                    if (pin == temp[i]){
+                        check = true;
+                        Intent intent = new Intent(MainActivity.this, AdminMenu.class);
+                        startActivity(intent);
+                        break;
+                    }
+                    else {
+                        check = false;
+                    }
+                }
+
+
+            }
+        });
     }
 }
